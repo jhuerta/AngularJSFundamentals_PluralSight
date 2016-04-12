a616669e26fbceae2bdf90c6267322a4a44b6fa3
Juan Huerta <juan.huerta@upu.int>
2016-04-12 11:40:47 +0200

    Testing the controller II

---
+++ test/unit/EditProfileControllerSpec.js
@@ -0,0 +1,34 @@
+'use strict';
+
+describe('EditProfileControllerSpec', EditProfileControllerTest);
+
+function EditProfileControllerTest() {
+    var $controllerConstructor, scope, mockGravatarUrlBuilder;
+
+
+    beforeEach(module('eventsApp'));
+
+    beforeEach(inject(function($controller, $rootScope) {
+        $controllerConstructor = $controller;
+        scope = $rootScope.$new();
+
+        mockGravatarUrlBuilder = sinon.stub({
+        	buildGravatarUrl: function(){}
+        });
+    }));
+
+
+    it('should build the gravatar given an email', function() {
+        $controllerConstructor('EditProfileController', {
+            '$scope': scope,
+            gravatarUrlBuilder: mockGravatarUrlBuilder
+        });
+
+        var email = "test@test.com";
+
+        scope.getGravatarUrl(email);
+
+        expect(mockGravatarUrlBuilder.buildGravatarUrl.calledWith(email)).toBe(true);
+        
+    });
+}
